0571905276ffefbaab92fbf3b7ad06dcbbee91f6
剱持遥太郎 <harutarokenmotsu@Air.local>
2022-05-15 23:00:41 +0900

    add new file

---
+++ sample1.java
@@ -0,0 +1,17 @@
+class sample1 {
+    public static void main(String[] args) {
+        /* if文による条件分岐 */
+        // 温度
+        int temperature = 35;
+        // 温度が10度以下なら寒い
+        if (temperature <= 10) {   
+            System.out.println("寒いです。"); 
+            // 30度以上なら暑い 
+        } else if (temperature >= 30) {
+            System.out.println("暑いです。");
+            // 11度〜29度なら快適
+        } else {
+            System.out.println("快適です。");
+        }           
+    }
+}
